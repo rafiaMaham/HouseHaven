8333aa0bdf8329e533b5f0c2da204a583844e576
rafiakhtarmaham <rafiaakhtarmaham@gmail.com>
2024-02-29 11:48:35 +0530

    middleware and function to handle possible errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,10 +1,17 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
-  await newUser.save();
-  res.status(201).json("user created successfully");
+
+  try {
+    await newUser.save();
+    res.status(201).json("user created successfully");
+  } catch (error) {
+    next(error);
+    // next(errorHandler(550, "error from the customized function"));
+  }
 };
