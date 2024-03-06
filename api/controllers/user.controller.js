41fe839f093044c80713a95b9a06d8e3de300584
rafiakhtarmaham <rafiaakhtarmaham@gmail.com>
2024-03-06 07:42:16 +0530

    get user listings

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import { errorHandler } from "../utils/error.js";
 import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
+import Listing from "../models/listing.model.js"
 
 export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.id)
@@ -43,3 +44,16 @@ export const deleteUser = async (req, res, next) => {
   }
 };
 
+
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, "You can only view your own listings!"));
+  }
+};
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,7 +1,7 @@
 import express from "express";
 import {
   deleteUser,
-  
+  getUserListings,
   updateUser,
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
@@ -10,5 +10,6 @@ const router = express.Router();
 
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/listings/:id", verifyToken, getUserListings);
 
 export default router;
