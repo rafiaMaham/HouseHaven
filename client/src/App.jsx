b25c3fa78ac18e7c4bc60eecec003e235ac1a8d9
rafiakhtarmaham <rafiaakhtarmaham@gmail.com>
2024-03-06 22:23:28 +0530

    image slider functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
 import UpdateListing from "./pages/UpdateListing";
+import Listing from "./pages/Listing";
 
 const App = () => {
   return (
@@ -17,12 +18,13 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/listing/:listingId" element={<Listing />} />
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
           <Route path="create-listing" element={<CreateListing />} />
-          <Route path="update-listing/:listingId" element={<UpdateListing/>} />
+          <Route path="update-listing/:listingId" element={<UpdateListing />} />
         </Route>
-5 
+        5
         <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
