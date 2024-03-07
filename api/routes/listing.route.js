810522a0a71c1542787c1e37b75d6addfd394f8b
rafiakhtarmaham <rafiaakhtarmaham@gmail.com>
2024-03-07 18:34:50 +0530

    created search API route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -3,6 +3,7 @@ import {
   createListing,
   deleteListing,
   getListing,
+  getListings,
   updateListing,
 } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
@@ -11,5 +12,7 @@ const router = express.Router();
 router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
 router.post("/update/:id", verifyToken, updateListing);
-router.get('/get/:id', getListing);
+router.get("/get/:id", getListing);
+router.get("/get", getListings);
+
 export default router;
