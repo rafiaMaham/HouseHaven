28e42473e2686f9990eff0d9e77c464ba5b39583
rafiakhtarmaham <rafiaakhtarmaham@gmail.com>
2024-03-08 20:36:14 +0530

    fix6

--- api/index.js
+++ api/index.js
@@ -6,14 +6,12 @@ import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
 import path from "path";
-
 dotenv.config();
-
 
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
-    console.log("database is connected");
+    console.log("Connected to MongoDB!");
   })
   .catch((err) => {
     console.log(err);
@@ -21,14 +19,14 @@ mongoose
 
 const __dirname = path.resolve();
 
-
 const app = express();
 
 app.use(express.json());
+
 app.use(cookieParser());
 
 app.listen(3000, () => {
-  console.log("server is running at port 3000");
+  console.log("Server is running on port 3000!");
 });
 
 app.use("/api/user", userRouter);
@@ -41,10 +39,9 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
-//middleware
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || "Internal server error...";
+  const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
     success: false,
     statusCode,
